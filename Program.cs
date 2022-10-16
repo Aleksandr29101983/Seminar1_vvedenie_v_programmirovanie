c5a14d3eb4cfb478c94efe91ad76e5840cf0798f
Aleksandr <kossander@bk.ru>
2022-10-16 22:53:54 +0300

    add Seminar1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,37 @@
-﻿
+﻿//Console.Write("Input int number: ");
+//int num = Convert.ToInt32(Console.ReadLine());
+//int rezult = num * num;
+//Console.WriteLine($"Quard of {num} is {rezult}");
+//Console.WriteLine(num);
+//Console.WriteLine("It's new number " + num + " and it's good");
+//Console.WriteLine($"It's new number {num} and it's good");
+//Задача 2. Напишите программу, которая на вход два числа и проверяет, является ли первое число квадратом второго
+//Console.Write("Input first int number: ");
+//int num1 = Convert.ToInt32(Console.ReadLine());
+//Console.Write("Input second int number: ");
+//int num2 = Convert.ToInt32(Console.ReadLine());
+
+//if (num2 * num2 == num1)
+//{
+//    Console.WriteLine($"{num1} is a quard of {num2}");
+//}
+//else
+//{
+//    Console.WriteLine($"{num1} is not a quard of {num2}");
+//}
+//Задача 3. Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа все целые числа в промежутке от минус этого числа до этого числа
+
+//Console.Write("Input int number: ");
+//int num = Convert.ToInt32(Console.ReadLine());
+//int current = (-1) * num;
+//while (current <= num)
+//{
+//    Console.WriteLine(current + " ");
+//    current = current + 1;
+//}
+//for(int i = current; i <= num; i = i + 1)
+//{
+//    Console.Write(i + "  ");
+//}
+
+//Задача 4. Напишите программу, которая принимает на вход трехзначное число и на выходе показывает последнюю цифру этого числа.
