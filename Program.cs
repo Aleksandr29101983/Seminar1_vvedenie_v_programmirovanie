7e33bf6a4c5fece6533a7c045a06ba7ad800564e
Aleksandr <kossander@bk.ru>
2022-10-17 23:07:06 +0300

    Homework for the seminar1

--- Program.cs
+++ Program.cs
@@ -1,4 +1,91 @@
-﻿//Console.Write("Input int number: ");
+﻿//Zадача 2: Напишите программу, которая на вход принимает два числа и выдает, 
+          //какое число большее, а какое меньшее
+
+// Console.Write("Input first number: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input second number: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// if (num1 > num2)
+//{
+ //   Console.WriteLine($"The larger number is {num1}, and the smaller number is {num2}");
+//}
+//else
+//{
+//    Console.WriteLine($"The larger number is {num2}, and the smaller number is {num1}");
+//}
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдает
+//           максимальное из этих чисел
+                                  
+// Console.Write("Enter the first number: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Enter the second number: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Enter the third number: ");
+// int num3 = Convert.ToInt32(Console.ReadLine());
+// int max = num1;
+
+// if (num2 > max)
+// {
+    // max = num2;
+// }
+
+// if (num3 > max)
+// {
+    // max = num3;
+// }
+
+// Console.WriteLine($"The maximum numbers is {max}");
+
+
+// Задача 6: Напишите программу, которая на вход принимает число и выдает,
+//           является ли число четным (делится ли оно на два без остатка)
+
+// Console.Write("Enter number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// if ((num % 2) == 0)
+// {
+//     Console.WriteLine($"The number {num} is even");
+// }
+// else
+// {
+//     Console.WriteLine($"The number {num} is not even");
+// }
+
+// Задача 8: Напишите программу, которая на вход принимает число (N),
+//           а на выходе показывает все четные числа от 1 до N
+
+// Console.Write("Enter number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int temp = 2;
+
+// while (temp <= num)
+// {
+//     Console.WriteLine(temp);
+//     temp = temp + 2;
+// }
+
+
+
+
+                               
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Console.Write("Input int number: ");
 //int num = Convert.ToInt32(Console.ReadLine());
 //int rezult = num * num;
 //Console.WriteLine($"Quard of {num} is {rezult}");
@@ -35,3 +122,9 @@
 //}
 
 //Задача 4. Напишите программу, которая принимает на вход трехзначное число и на выходе показывает последнюю цифру этого числа.
+
+//Console.Write("Input three-digit int number: ");
+//nt num = Convert.ToInt32(Console.ReadLine());
+
+//int result = num % 10;
+//Console.WriteLine($"Last digit of {num} is {result}");
